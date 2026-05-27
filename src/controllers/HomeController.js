f2cf634fccfd70c5f3052592d113c7373331c281
Igor Sampaio <isampaiodequeiroz@gmail.com>
2026-05-27 17:57:31 -0300

    criação da rota e controller de alunos

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-  const novoAluno = await Aluno.create({
-    nome: 'Igor',
-    sobrenome: 'Sampaio',
-    email: 'isampaiodequeiroz@gmail.com',
-    idade: 34,
-    peso: 93.4,
-    altura:1.77
-  });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
