ab70a44a06bb71012c3411947c212cf9bacac5f8
Igor Sampaio <isampaiodequeiroz@gmail.com>
2026-05-28 17:17:06 -0300

    prepara para deploy

--- src/models/Foto.js
+++ src/models/Foto.js
@@ -25,7 +25,7 @@ export default class Foto extends Model {
             url: {
                 type: Sequelize.VIRTUAL,
                 get() {
-                    return `${appConfig}/images/${this.getDataValue('filename')}`;
+                    return `${appConfig.url}/images/${this.getDataValue('filename')}`;
                 }
             }
         },
